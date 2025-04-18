27561907694a22142685be9e4e1826148ebc42c0
sbxchicago <junkmail@sandboxstudio.net>
2025-04-18 11:34:20 -0500

    lunr update

--- _site/assets/js/search.js
+++ _site/assets/js/search.js
@@ -74,7 +74,7 @@
             }
             item.title = markedTitle.join("");
           }
-          console.log(item.title + ' ' + firstMatch);
+
           appendString += '<li><a href="' + item.url + '"><h3>' + item.title + '</h3></a>';
           appendString += '<p>&hellip;' + item.content.substring(firstMatch-25, firstMatch+125) + '&hellip;</p></li>';    
         }     
@@ -128,15 +128,149 @@
       };
     });
 
-    searchTerm = '+'+searchTerm;
-    searchTerm = searchTerm.replace(' ', ' +');
-    // searchTerm = searchTerm.replace(" ", "\ ");
-    // searchTerm = searchTerm.replace(" ", "*");
 
+    // remove the lunr stop words from query
+    let stopWords = [
+      'a',
+      'able',
+      'about',
+      'across',
+      'after',
+      'all',
+      'almost',
+      'also',
+      'am',
+      'among',
+      'an',
+      'and',
+      'any',
+      'are',
+      'as',
+      'at',
+      'be',
+      'because',
+      'been',
+      'but',
+      'by',
+      'can',
+      'cannot',
+      'could',
+      'dear',
+      'did',
+      'do',
+      'does',
+      'either',
+      'else',
+      'ever',
+      'every',
+      'for',
+      'from',
+      'get',
+      'got',
+      'had',
+      'has',
+      'have',
+      'he',
+      'her',
+      'hers',
+      'him',
+      'his',
+      'how',
+      'however',
+      'i',
+      'if',
+      'in',
+      'into',
+      'is',
+      'it',
+      'its',
+      'just',
+      'least',
+      'let',
+      'like',
+      'likely',
+      'may',
+      'me',
+      'might',
+      'most',
+      'must',
+      'my',
+      'neither',
+      'no',
+      'nor',
+      'not',
+      'of',
+      'off',
+      'often',
+      'on',
+      'only',
+      'or',
+      'other',
+      'our',
+      'own',
+      'rather',
+      'said',
+      'say',
+      'says',
+      'she',
+      'should',
+      'since',
+      'so',
+      'some',
+      'than',
+      'that',
+      'the',
+      'their',
+      'them',
+      'then',
+      'there',
+      'these',
+      'they',
+      'this',
+      'tis',
+      'to',
+      'too',
+      'twas',
+      'us',
+      'wants',
+      'was',
+      'we',
+      'were',
+      'what',
+      'when',
+      'where',
+      'which',
+      'while',
+      'who',
+      'whom',
+      'why',
+      'will',
+      'with',
+      'would',
+      'yet',
+      'you',
+      'your'
+    ];
+
+
+    stopWords.forEach( function(term){ 
+      let re_beg =  new RegExp( '^'+term+'\\s', 'g' );   
+      searchTerm = searchTerm.replace(re_beg, '');
+
+      let re_end =  new RegExp( '\\s'+term+'$', 'g' ); 
+      searchTerm = searchTerm.replace(re_end, '');
+
+      let re_spaces =  new RegExp( '\\s+'+term+'\\s+', 'g' );            
+      searchTerm = searchTerm.replace(re_spaces, ' ');
+    });
+
+
+    //require the remaining terms 
+    searchTerm = '+'+searchTerm;
+    searchTerm = searchTerm.replace(/\s+/, ' +');
+   
     var results = idx.search(searchTerm); // Get lunr to perform a search
-    // var results = idx.query(function () {
-    //   this.term(searchTerm)
-    // });
+
     
     displaySearchResults(results, window.store); 
 
